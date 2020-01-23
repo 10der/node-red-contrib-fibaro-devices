206c71e2137ddb1d58672bdc9354849d7c9b0bcf
Oleg Denisenko <oleg.denisenko@gmail.com>
2020-01-23 20:31:22 +0200

    Update fibaroQueryState.js
    minor fix

--- fibaroQueryState.js
+++ fibaroQueryState.js
@@ -4,7 +4,7 @@ module.exports = function (RED) {
         this.server = n.server;
         this.serverConfig = RED.nodes.getNode(this.server);
         var node = this;
-        var fibaro = this.serverConfig.client;        
+        var fibaro = this.serverConfig.client;
         node.status({});
 
         if (this.serverConfig) {
@@ -16,15 +16,13 @@ module.exports = function (RED) {
         }
 
         node.on('input', function (msg) {
-            if (msg.payload) {
-                var deviceID = msg.topic.split("/").reverse()[0];
-                var property = msg.property || 'value';
-                fibaro.queryState(deviceID, property, (currentState) => {
-                    msg.currentState = currentState;
-                    node.send(msg);
-                    node.status({});
-                }, (error) => node.status({ fill: "red", shape: "dot", text: error.text }));
-            }
+            var deviceID = msg.topic.split("/").reverse()[0];
+            var property = msg.property || 'value';
+            fibaro.queryState(deviceID, property, (currentState) => {
+                msg.currentState = currentState;
+                node.send(msg);
+                node.status({});
+            }, (error) => node.status({ fill: "red", shape: "dot", text: error.text }));
         });
     }
 
